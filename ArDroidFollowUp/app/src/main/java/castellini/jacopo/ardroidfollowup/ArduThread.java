c0e4d70b9e70680ac33f1bec39c9e2b2845cdaa1
opocaj92 <jacopo.castellini@gmail.com>
2015-06-23 14:41:18 +0200

    Ora la comunicazione con Arduino funziona!

---
+++ ArDroidFollowUp/app/src/main/java/castellini/jacopo/ardroidfollowup/ArduThread.java
@@ -0,0 +1,57 @@
+package castellini.jacopo.ardroidfollowup;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ArduThread extends Thread {
+    String command;
+    FileOutputStream mOutputStream;
+    FileInputStream mInputStream;
+
+    public ArduThread(String command, FileOutputStream mOutputStream, FileInputStream mInputStream) {
+        this.command = command;
+        this.mOutputStream = mOutputStream;
+        this.mInputStream = mInputStream;
+    }
+
+    public void run() {
+        sendCommand();
+    }
+
+    private boolean sendCommand() {
+        byte[] buffer = "null".getBytes();
+        if (command.equals("stop"))
+            buffer = "0V;".getBytes();
+        else if (command.equals("left"))
+            buffer = "0V;1V0;2A3;2B2;".getBytes();
+        else if (command.equals("right"))
+            buffer = "0V;1V0;2A2;2B3;".getBytes();
+        else if (command.equals("forward"))
+            buffer = "0V;1V1;2V3;".getBytes();
+        else if (command.equals("backward"))
+            buffer = "0V;1V0;2V3;".getBytes();
+
+        if (mOutputStream != null) {
+            try {
+                mOutputStream.write(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (mInputStream != null) {
+            int ret = 0;
+            byte[] response = new byte[2];
+            while (ret <= 0) {
+                try {
+                    ret = mInputStream.read(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return response[0] == '4' && response[1] == '2';
+        }
+        return false;
+    }
+
+}
---
+++ ArDroidFollowUp/app/src/main/java/castellini/jacopo/ardroidfollowup/MainActivity.java
@@ -0,0 +1,182 @@
+package castellini.jacopo.ardroidfollowup;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String ACTION_USB_PERMISSION = "castellini.jacopo.ardroidfollowup.usb_permission";
+
+    static {
+        OpenCVLoader.initDebug();
+    }
+
+    UsbAccessory mAccessory;
+    ParcelFileDescriptor mFileDescriptor;
+    FileInputStream mInputStream;
+    FileOutputStream mOutputStream;
+    private UsbManager mUsbManager;
+    private PendingIntent mPermissionIntent;
+    private boolean mPermissionRequestPending;
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbAccessory accessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false))
+                        openAccessory(accessory);
+                    mPermissionRequestPending = false;
+                }
+            } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+                UsbAccessory accessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+                if (accessory != null && accessory.equals(mAccessory))
+                    closeAccessory();
+            }
+        }
+    };
+    private CameraBridgeViewBase mOpenCvCameraView;
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+    private String currState = "null";
+    private Mat mRgba;
+    private FeatureScanner featureScanner;
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+        closeAccessory();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+
+        if (mInputStream == null || mOutputStream == null) {
+            UsbAccessory[] accessories = mUsbManager.getAccessoryList();
+            UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+            if (accessory != null) {
+                if (mUsbManager.hasPermission(accessory))
+                    openAccessory(accessory);
+                else {
+                    synchronized (mUsbReceiver) {
+                        if (!mPermissionRequestPending) {
+                            mUsbManager.requestPermission(accessory, mPermissionIntent);
+                            mPermissionRequestPending = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void onDestroy() {
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+        unregisterReceiver(mUsbReceiver);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        featureScanner = new FeatureScanner(this.getApplicationContext());
+
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+        registerReceiver(mUsbReceiver, filter);
+
+        setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.cameraSurface);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    private void openAccessory(UsbAccessory accessory) {
+        mFileDescriptor = mUsbManager.openAccessory(accessory);
+        if (mFileDescriptor != null) {
+            mAccessory = accessory;
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            mInputStream = new FileInputStream(fd);
+            mOutputStream = new FileOutputStream(fd);
+        }
+    }
+
+    private void closeAccessory() {
+        if (mFileDescriptor != null) {
+            try {
+                mFileDescriptor.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mFileDescriptor = null;
+                mAccessory = null;
+            }
+        }
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+
+        String newState = featureScanner.doTheWork(mRgba);
+
+        if (!newState.equals(currState)) {
+            currState = newState;
+            ArduThread t = new ArduThread(newState, mOutputStream, mInputStream);
+            t.start();
+        }
+
+        return mRgba;
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat();
+    }
+
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+}
